212591a456816b5c9af8f951336f479a2eef267c
gaurav21r <gaurav@rcorp.co.in>
2016-02-16 02:35:02 +0530

    Stable BUild

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,25 +10,29 @@ module.exports = function(grunt) {
             // Optionally can be used via Command line.
             // `browserify -d -s jequel -t coffeeify --bare src\index.coffee > src\index.js`
 
-            options: {
-                transform: [['coffeeify', {
-                    bare: true
-                }]],
-                //extension: 'coffee',
-                browserifyOptions: {
-                    debug: true,
-                    standalone: 'jequel'
-                },
-                exclude: 'underscore'
-            },
             dist: {
                 files: {
                     'dist/index.js': ['src/index.coffee']
-                }
+                },
+				options: {
+	                transform: [['coffeeify', {
+	                    bare: true
+	                }]],
+	                //extension: 'coffee',
+	                browserifyOptions: {
+	                    debug: true,
+	                    standalone: 'jequel'
+	                },
+					debug: true,
+					bare: true,
+					watch: true,
+					keepAlive: true,
+					exclude: ['underscore']
+	            }
             }
         }
 	})
 	grunt.loadNpmTasks('grunt-docco');
     grunt.loadNpmTasks('grunt-browserify');
-	grunt.registerTask('default', ['docco']);
+	grunt.registerTask('default', ['browserify:dist']);
 }
